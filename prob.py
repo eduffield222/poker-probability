b143fecad9249bc24da3b3e88655297d4669bc8c
Evan Duffield <evan@uberbox.(none)>
2013-03-06 22:59:53 -0500

    added basic input

--- prob.py
+++ prob.py
@@ -7,8 +7,15 @@ from time import time
 from deck import Deck
 from card import Card
 from hand import Hand
+from re import split
 
 deck = Deck()
+
+cards = raw_input("Enter your cards (7c As): ")
+community = raw_input("Enter first 3 community cards (3s As Ts): ")
+
+cards_split = split(' ', cards)
+community_split = split(' ', community)
 
 start = time()
 total = 10000.0
@@ -22,15 +29,15 @@ for i in xrange(int(total)):
     deck.get_new_deck()
 
     community_cards = []
-    community_cards.append (deck.deal())
-    community_cards.append (deck.deal())
-    community_cards.append (deck.deal())
+    community_cards.append (Card(community_split[0][0], community_split[0][1]))
+    community_cards.append (Card(community_split[1][0], community_split[1][1]))
+    community_cards.append (Card(community_split[2][0], community_split[2][1]))
     community_cards.append (deck.deal())
     community_cards.append (deck.deal())
 
     my_hand = Hand(community_cards)
-    my_hand.add (deck.deal())
-    my_hand.add (deck.deal())
+    my_hand.add (Card(cards_split[0][0], cards_split[0][1]))
+    my_hand.add (Card(cards_split[1][0], cards_split[1][1]))
 
     #print my_hand.display_cards()
     #print "IS FOUR OF A KIND", my_hand.four_of_a_kind()
